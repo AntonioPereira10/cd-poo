aadb9c7242c3c57c5ffd1ea2125202796af504e1
AntonioPereira10 <antonio.barro@campusdual.com>
2023-10-19 09:26:21 +0200

    Ejercicio 1

---
+++ src/main/java/com/campusdual/exercisespoo/Exercise01.java
@@ -0,0 +1,27 @@
+package com.campusdual.exercisespoo;
+
+import java.sql.SQLOutput;
+
+public class Exercise01 {
+
+    public static void main(String[] args) {
+       System.out.println("Hello, World!");
+//       System.out.println(3+5);
+//       System.out.println(3-2);
+//       System.out.println(4*3);
+//       System.out.println(5/2);
+//       System.out.println(5%2);
+//       escribir sout
+//       Ejercicios dia 18/10/2023
+//       String cnv = "200";
+//       int cnvInt = Integer.parseInt(cnv);
+//       cnvInt = cnvInt + 1;
+//       String cnvStr = String.valueOf(cnvInt);
+//       char cnvChar = cnvStr.charAt(2);
+//       0 = "0" = 0.0 = false = "false"
+//       2 = "2" = 2.00000 = true = "true"
+
+
+    }
+}
+
