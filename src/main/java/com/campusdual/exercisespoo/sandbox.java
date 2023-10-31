e174e58340d512a36ebd38abdb37b0d1e3466e46
AntonioPereira10 <antonio.barro@campusdual.com>
2023-10-31 10:32:24 +0100

    coche

--- src/main/java/com/campusdual/exercisespoo/sandbox.java
+++ src/main/java/com/campusdual/exercisespoo/sandbox.java
@@ -4,6 +4,8 @@ public class sandbox {
 
     public static void main(String[] args) {
         perro perro1= new perro(90, false,"Husky","Balto","Negro");
+
+        perro1.ladrar();
         perro1.presentarse();
     }
 }
