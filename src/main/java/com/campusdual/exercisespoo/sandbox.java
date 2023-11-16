19b1ad02136311078eb11f946eab1a2106ec18cd
AntonioPereira10 <antonio.barro@campusdual.com>
2023-11-16 09:13:07 +0100

    json

--- src/main/java/com/campusdual/exercisespoo/sandbox.java
+++ src/main/java/com/campusdual/exercisespoo/sandbox.java
@@ -8,7 +8,7 @@ import com.campusdual.exercisespoo.exercise14.Teacher;
 public class sandbox {
 
     public static void main(String[] args) {
-        perro perro1 = new perro(90, false, "Husky", "Balto", "Negro");
+        Perro perro1 = new Perro(90, false, "Husky", "Balto", "Negro");
 
         perro1.ladrar();
         perro1.presentarse();
